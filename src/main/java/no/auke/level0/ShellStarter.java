9414fa162f2f8fde0df2a3a6542c4695da592a8c
Thor Henning Hetland <totto@totto.org>
2011-10-04 18:53:48 +0200

    added gitignore

--- src/main/java/no/auke/level0/ShellStarter.java
+++ src/main/java/no/auke/level0/ShellStarter.java
@@ -1,18 +1,33 @@
 package no.auke.level0;
 
 
+import net.jxta.platform.NetworkManager;
 
+import java.text.MessageFormat;
 
 public class ShellStarter {
 
 
-    public ShellStarter() {
+    public static void main(String args[]) {
+        System.setProperty("net.jxta.logging.Logging", "FINEST");
+        System.setProperty("net.jxta.level", "FINEST");
+        System.setProperty("java.util.logging.config.file", "logging.properties");
 
-
-    }
-
-    public static void main(String[] args) throws Exception {
-
+        NetworkManager manager = null;
+        try {
+            manager = new NetworkManager(NetworkManager.ConfigMode.EDGE, "HelloWorld");
+            System.out.println("Starting JXTA");
+            manager.startNetwork();
+            System.out.println("JXTA Started");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        System.out.println("Waiting for a rendezvous connection");
+        boolean connected = manager.waitForRendezvousConnection(12000);
+        System.out.println(MessageFormat.format("Connected :{0}", connected));
+        System.out.println("Stopping JXTA");
+        manager.stopNetwork();
     }
 
 }
